6292a1cd500933bcce7dffdf03bb2fac69a56b21
Vikram Bankar <vkmbankar@gmail.com>
2023-03-20 10:08:50 +0530

    All tested updated

---
+++ model/prices.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from model.crud import base
+
+
+class Prices(base):
+    __tablename__ = 'prices'
+    id = Column(Integer, primary_key=True)
+    contractor_id = Column(Integer, ForeignKey('contractors.id'), nullable=False)
+    project_type_id = Column(Integer, ForeignKey('project_types.id'), nullable=False)
+    price_per_unit = Column(String)
+
+    def __repr__(self):
+        return "<id(id='{}', contractor_id='{}', project_type_id='{}', price_per_unit={})>" \
+            .format(self.id, self.contractor_id, self.project_type_id, self.price_per_unit)
---
+++ model/project_types.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+
+from model.crud import base
+
+
+class ProjectTypes(base):
+    __tablename__ = 'project_types'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    materials = Column(String)
+    units = Column(String)
+
+    def __repr__(self):
+        return "<id(id='{}', name='{}', description='{}', materials={}, units={})>" \
+            .format(self.id, self.name, self.description, self.materials, self.units)
